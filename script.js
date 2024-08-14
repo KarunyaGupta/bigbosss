0e86a8988b0f01de6c6d5911a160ae4fad1ae556
KarunyaGupta <mails.karunyagupta@gmail.com>
2024-08-14 08:54:58 +0530

    update some features

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-    alert("This site uses Cookies")
-    let top_button=document.getElementById('top_button');
-    window.onscroll=function(){
-        scroll();
-    }
-    function scroll(){
-        if (document.documentElement.scrollTop>500|document.body.scrollTop>500){
-            top_button.style.display='block'
-        }
-        else{
-            top_button.style.display='none'
-        }
-    }
-    function topfunction(){
-        document.body.scrollTop=0;
-        document.documentElement.scrollTop=0;
-    }
+alert("THIS WEBSITE IS NOT RESPONSIVE RECOMENDED ON YOUR LAPTOP AND DESKTOP ");
